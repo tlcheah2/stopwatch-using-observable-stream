e56d30eed4c210e9c5ba88f4726cbfbadb0a35b9
Tek Loon <tekloon.1991@gmail.com>
2021-10-29 22:20:24 +0800

    feat: using fromEvent to capture click listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stopwatch-using-observable-stream';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('btnStart') btnStart!: ElementRef<HTMLButtonElement>;
+  @ViewChild('btnStop') btnStop!: ElementRef<HTMLButtonElement>;
+  @ViewChild('btnReset') btnReset!: ElementRef<HTMLButtonElement>;
+
+  ngAfterViewInit() {
+    this.initClickListener();
+  }
+
+  initClickListener() {
+    fromEvent(this.btnStart.nativeElement, 'click')
+      .subscribe(() => this.startTimer());
+    fromEvent(this.btnStop.nativeElement, 'click')
+      .subscribe(() => this.stopTimer());
+    fromEvent(this.btnReset.nativeElement, 'click')
+      .subscribe(() => this.resetTimer());
+  }
+
+  startTimer() {
+    console.log('start btn clicked')
+  }
+
+  stopTimer() {
+    console.log('stop btn clicked')
+  }
+
+  resetTimer() {
+    console.log('reset btn clicked')
+  }
+
+  
+
+
 }
