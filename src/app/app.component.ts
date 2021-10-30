c223ad3e8229fb28fe53c9f66af44385265f5845
Tek Loon <tekloon.1991@gmail.com>
2021-10-30 22:09:53 +0800

    feat: build stopwatch using merge, switchMap, tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, Observable, of, Subscription } from 'rxjs';
+import { mapTo, switchMap, tap } from 'rxjs/operators';
+
+
+enum StopwatchAction {
+  START_TIMER = 'START_TIMER',
+  STOP_TIMER = 'STOP_TIMER',
+  RESET_TIMER = 'RESET_TIMER'
+}
+
+interface StopwatchState {
+  isCounting: boolean;
+  value: number;
+}
+
+ const SECONDS_IN_HOUR = 3600;
+ const SECONDS_IN_MINUTE = 60;
+
 
 @Component({
   selector: 'app-root',
@@ -11,32 +28,73 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('btnStop') btnStop!: ElementRef<HTMLButtonElement>;
   @ViewChild('btnReset') btnReset!: ElementRef<HTMLButtonElement>;
 
+  state: StopwatchState;
+  userEvents: Observable<StopwatchAction | StopwatchAction | StopwatchAction> | undefined;
+  userEventsSubscription: Subscription | undefined;
+
+  constructor() {
+    this.state = {
+      isCounting: false,
+      value: 0
+    }
+  }
+
   ngAfterViewInit() {
     this.initClickListener();
   }
 
+  formatTimerDisplay(seconds: number) {
+    
+    const hours: number = Math.floor(seconds / SECONDS_IN_HOUR );
+    let remainingSeconds: number = seconds - (hours * SECONDS_IN_HOUR);
+
+    const minutes: number = Math.floor(remainingSeconds / SECONDS_IN_MINUTE);
+    remainingSeconds -= (minutes * SECONDS_IN_MINUTE);
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+  }
+
   initClickListener() {
-    fromEvent(this.btnStart.nativeElement, 'click')
-      .subscribe(() => this.startTimer());
-    fromEvent(this.btnStop.nativeElement, 'click')
-      .subscribe(() => this.stopTimer());
-    fromEvent(this.btnReset.nativeElement, 'click')
-      .subscribe(() => this.resetTimer());
+    // Merge all the user click event so we could centralize the state change later
+    this.userEvents = merge(
+      fromEvent(this.btnStart.nativeElement, 'click').pipe(mapTo(StopwatchAction.START_TIMER)),
+      fromEvent(this.btnStop.nativeElement, 'click').pipe(mapTo(StopwatchAction.STOP_TIMER)),
+      fromEvent(this.btnReset.nativeElement, 'click').pipe(mapTo(StopwatchAction.RESET_TIMER))
+    )
+
+    this.userEventsSubscription = this.userEvents.pipe(
+      // Using switchMap here because it can restart the interval operator in the switch case when there is new click event.
+      // This is perfect for stopwatch scenario
+      switchMap((action) => this.handleEventAction(action))
+    ).subscribe();
   }
 
-  startTimer() {
-    console.log('start btn clicked')
+  ngOnDestroy() {
+    this.userEventsSubscription?.unsubscribe();
   }
 
-  stopTimer() {
-    console.log('stop btn clicked')
+  handleEventAction(action: StopwatchAction): Observable<any> {
+    switch (action) {
+      case StopwatchAction.START_TIMER: {
+        return interval(1000).pipe(
+          tap(_ => this.updateState({ isCounting: true, value: this.state.value + 1 }))
+        )
+      }
+      case StopwatchAction.STOP_TIMER: {
+        this.updateState({ isCounting: false, value: this.state.value });
+        return NEVER;
+      }
+      case StopwatchAction.RESET_TIMER: {
+        this.updateState({ isCounting: false, value: 0 });
+        return NEVER;
+      }
+      default:
+        return NEVER;
+    }
   }
-
-  resetTimer() {
-    console.log('reset btn clicked')
-  }
-
   
-
-
+  updateState(state: StopwatchState) {
+    this.state = { ...state };
+    console.log('Updated State', this.state);
+  }
 }
